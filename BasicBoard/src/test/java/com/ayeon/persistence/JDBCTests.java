77ebf0a9195da53657608d52ab77b31b1b107b42
nam-ayeon <boalove284@naver.com>
2023-02-20 13:14:20 +0900

    connectionPoolTest_success

--- BasicBoard/src/test/java/com/ayeon/persistence/JDBCTests.java
+++ BasicBoard/src/test/java/com/ayeon/persistence/JDBCTests.java
@@ -3,13 +3,25 @@ package com.ayeon.persistence;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
-import org.junit.Test;
+import javax.sql.DataSource;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import lombok.Setter;
 import lombok.extern.log4j.Log4j;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
 @Log4j
 public class JDBCTests {
 
+	@Setter(onMethod_ = @Autowired)
+	DataSource dataSource;
+	
 	static {
 
 		try {
@@ -23,19 +35,34 @@ public class JDBCTests {
 
 	}
 	
-	
 	@Test
-	public void testConnection() {
-		
+	public void testConnectionPool() throws Exception{
 		try {
-			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/legacyProject?serverTimezone=Asia/Seoul",
-					"root",
-					"mysql");
-			
+			Connection conn = dataSource.getConnection();
 			log.info("conn" + conn);
+			Thread.sleep(1000);
 		} catch (Exception e) {
-			log.info(" 커넥션 얻기실패 " + e.getMessage());
+			log.info("커넥션얻기 실패: " + e.getMessage());
 		}
 		
 	}
+	
+	
+//	@Test
+//	public void testConnection() {
+//		
+//		try {
+//			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/legacyProject?serverTimezone=Asia/Seoul",
+//					"root",
+//					"mysql");
+//			
+//			log.info("conn" + conn);
+//		} catch (Exception e) {
+//			log.info(" 커넥션 얻기실패 " + e.getMessage());
+//		}
+//		
+//	}
+	
+	
+	
 }
