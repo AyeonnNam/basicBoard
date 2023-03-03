ed5733d5000ed7b8439936e8d60182426c977009
nam-ayeon <boalove284@naver.com>
2023-03-03 23:42:02 +0900

    BoardVO 클래스 작

--- BasicBoard/src/main/java/com/ayeon/domain/BoardVO.java
+++ BasicBoard/src/main/java/com/ayeon/domain/BoardVO.java
@@ -1,8 +1,18 @@
 package com.ayeon.domain;
 
+import java.util.Date;
+
 import lombok.Data;
 
 @Data
 public class BoardVO {
-
+	private Long bno;
+	private String title;
+	private String content;
+	private String writer;
+	private Date regdate;
+	private Date updatedate;
+	
+	
 }
+
