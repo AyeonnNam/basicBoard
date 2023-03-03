1ad7ae97ba1b5c8baf213b5c8d55c30421ce0e44
nam-ayeon <boalove284@naver.com>
2023-03-03 23:46:10 +0900

    root-context.xml - 매퍼패키지스캔설정BoardMapper 클래스 작성

---
+++ BasicBoard/src/main/java/com/ayeon/mapper/BoardMapper.java
@@ -0,0 +1,15 @@
+package com.ayeon.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Select;
+
+import com.ayeon.domain.BoardVO;
+
+public interface BoardMapper {
+
+	@Select("select * from tbl_board where bno >0 ")
+	public List<BoardVO> getList();
+	
+
+}
