f98928a191f6863e7f693c3fee6b3b4797b5b582
nam-ayeon <boalove284@naver.com>
2023-03-05 16:05:02 +0900

    BoardController

---
+++ BasicBoard/src/main/java/com/ayeon/goodWeb/BoardController.java
@@ -0,0 +1,31 @@
+package com.ayeon.goodWeb;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.ayeon.service.BoardService;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Controller
+@Slf4j
+@RequestMapping("/board/*")
+@AllArgsConstructor
+public class BoardController {
+
+	
+	private BoardService service;
+	
+	@GetMapping("/list")
+	public void list(Model model) {
+		
+		
+		log.info("list...............");
+		model.addAttribute("list",service.getList());
+		
+	}
+	
+}
