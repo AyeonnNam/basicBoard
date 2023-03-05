520c52c235bf77a57731fd93e706c04a8cf34caf
nam-ayeon <boalove284@naver.com>
2023-03-05 16:38:04 +0900

    @org.junit.Before 어노테이션 주

--- BasicBoard/src/main/java/com/ayeon/goodWeb/BoardController.java
+++ BasicBoard/src/main/java/com/ayeon/goodWeb/BoardController.java
@@ -24,7 +24,7 @@ public class BoardController {
 		
 		
 		log.info("list...............");
-		model.addAttribute("list",service.getList());
+		model.addAttribute("list", service.getList());
 		
 	}
 	
