c1c5b10fb044000301751ac858e3702fca733e29
nam-ayeon <boalove284@naver.com>
2023-03-10 22:46:50 +0900

    MyBatis, Spring 페이지 처리

--- BasicBoard/src/main/java/com/ayeon/domain/Criteria.java
+++ BasicBoard/src/main/java/com/ayeon/domain/Criteria.java
@@ -2,4 +2,21 @@ package com.ayeon.domain;
 
 public class Criteria {
 
+	private int pageNum;
+	private int amount;
+	
+	public Criteria() {
+		this(1,10);
+	}
+
+	public Criteria(int pageNum, int amount) {
+		// TODO Auto-generated constructor stub
+		this.pageNum = pageNum;
+		this.amount= amount;
+	}
+	
+	
+	
+	
+	
 }
