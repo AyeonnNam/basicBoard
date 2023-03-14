0a109acdd1cf32f4994b32e0226140f5aad273f6
nam-ayeon <boalove284@naver.com>
2023-03-15 01:50:20 +0900

    pageDTO

--- BasicBoard/src/main/java/com/ayeon/domain/pageDTO.java
+++ BasicBoard/src/main/java/com/ayeon/domain/pageDTO.java
@@ -17,16 +17,21 @@ public class pageDTO {
 	public pageDTO(Criteria cri, int total) {
 		
 		this.cri = cri;
-		this.total = total;
 		
-		this.endPage = (int)(Math.ceil(cri.getPageNum() / 10.0)) * 10;
-		this.startPage =this.endPage - 9;
-		int realEnd = (int) (Math.ceil((total*1.0)/cri.getAmount()));
+		this.total = total; 
+		
+		this.endPage = (int)(Math.ceil(cri.getPageNum() / 10.0)) * 10; 
+		
+		this.startPage = this.endPage - 9; 
+	
+		int realEnd = (int) (Math.ceil((total * 1.0)/cri.getAmount())); 
+		
 		if(realEnd < this.endPage) {
 			this.endPage = realEnd;
 		}
 		
 		this.prev = this.startPage > 1;
+		
 		this.next = this.endPage < realEnd;
 	}
 
