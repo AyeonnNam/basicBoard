70d02478710453a3a72bc8848873b7cd3caaae5f
nam-ayeon <boalove284@naver.com>
2023-03-23 22:05:03 +0900

    ReplyServiceImpl 클래스 작성

--- BasicBoard/src/main/java/com/ayeon/service/ReplyService.java
+++ BasicBoard/src/main/java/com/ayeon/service/ReplyService.java
@@ -9,7 +9,7 @@ public interface ReplyService {
 
 	public int register(ReplyVO vo);
 	
-	public ReplyVO get(Long vo);
+	public ReplyVO get(Long rno);
 	
 	public int modify(ReplyVO vo);
 	
---
+++ BasicBoard/src/main/java/com/ayeon/service/ReplyServiceImpl.java
@@ -0,0 +1,62 @@
+package com.ayeon.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ayeon.domain.Criteria;
+import com.ayeon.domain.ReplyVO;
+import com.ayeon.mapper.ReplyMapper;
+
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@Slf4j
+public class ReplyServiceImpl implements ReplyService {
+
+	@Setter(onMethod_ = @Autowired)
+	private ReplyMapper mapper;
+	
+	
+	@Override
+	public int register(ReplyVO vo) {
+		// TODO Auto-generated method stub
+		
+		log.info("register..........." +vo);
+		return mapper.insert(vo);
+	}
+
+	@Override
+	public ReplyVO get(Long rno) {
+		// TODO Auto-generated method stub
+		log.info("get............" + rno);
+		return mapper.read(rno);
+	}
+
+	@Override
+	public int modify(ReplyVO vo) {
+		// TODO Auto-generated method stub
+		
+		log.info("modify............" +  vo);
+		return mapper.update(vo);
+	}
+
+	@Override
+	public int remove(Long rno) {
+		// TODO Auto-generated method stub
+		log.info("remove.............." +  rno);
+		return mapper.delete(rno);
+	}
+
+	@Override
+	public List<ReplyVO> getList(Criteria cri, Long bno) {
+		// TODO Auto-generated method stub
+		log.info("get Reply List of a Board " +  bno);
+		return mapper.getListWithPaging(cri, bno);
+	}
+
+	
+		
+}
