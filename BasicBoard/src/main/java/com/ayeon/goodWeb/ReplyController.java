70ee71c54a9fdd3bae2347834562fd73ea32c76b
nam-ayeon <boalove284@naver.com>
2023-03-24 17:22:12 +0900

    특정게시물 댓글 목록 확인

--- BasicBoard/src/main/java/com/ayeon/goodWeb/ReplyController.java
+++ BasicBoard/src/main/java/com/ayeon/goodWeb/ReplyController.java
@@ -1,13 +1,20 @@
 package com.ayeon.goodWeb;
 
+import java.util.List;
+
+import javax.sql.rowset.serial.SerialArray;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ayeon.domain.Criteria;
 import com.ayeon.domain.ReplyVO;
 import com.ayeon.service.ReplyService;
 import com.ayeon.service.ReplyServiceImpl;
@@ -36,4 +43,19 @@ public class ReplyController {
 				new ResponseEntity<>("success", HttpStatus.OK) 
 				: new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
 	}
-}
+	
+	
+	@GetMapping(value="/pages/{bno}/{page}",
+		produces = {
+				MediaType.APPLICATION_XML_VALUE,
+				MediaType.APPLICATION_JSON_UTF8_VALUE})
+	public ResponseEntity<List<ReplyVO>> getList(@PathVariable("page") int page, @PathVariable("bno") Long bno){
+		log.info("getList............");
+		Criteria cri = new Criteria(page, 10);
+		log.info("cri" + cri);
+		return new ResponseEntity<List<ReplyVO>>(service.getList(cri, bno), HttpStatus.OK);
+		
+	}
+	
+	
+ }
