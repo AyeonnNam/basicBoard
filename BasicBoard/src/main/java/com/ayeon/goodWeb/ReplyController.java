49e0453074c00baff5075c2c1a8fe39f0231c692
nam-ayeon <boalove284@naver.com>
2023-03-24 17:41:12 +0900

    댓글 수정

--- BasicBoard/src/main/java/com/ayeon/goodWeb/ReplyController.java
+++ BasicBoard/src/main/java/com/ayeon/goodWeb/ReplyController.java
@@ -2,7 +2,6 @@ package com.ayeon.goodWeb;
 
 import java.util.List;
 
-import javax.sql.rowset.serial.SerialArray;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -13,6 +12,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ayeon.domain.Criteria;
@@ -65,4 +65,16 @@ public class ReplyController {
 				:new ResponseEntity<String>(HttpStatus.INTERNAL_SERVER_ERROR);
 }
 
+	@RequestMapping(method = {RequestMethod.PUT, RequestMethod.PATCH},
+			value="/{rno}", consumes = "application/json",
+			produces = {MediaType.TEXT_PLAIN_VALUE})
+	public ResponseEntity<String> modify(@PathVariable("rno") Long rno, @RequestBody ReplyVO vo){
+		vo.setRno(rno);
+		log.info("rno: ...................." + rno);
+		log.info("modify:................." + vo);
+		
+		return service.modify(vo) == 1 ? 
+				new ResponseEntity<String>("success", HttpStatus.OK):new ResponseEntity<String>(HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+	
 }
