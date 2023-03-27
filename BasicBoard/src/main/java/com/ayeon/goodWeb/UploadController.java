e69a46c4fd4b961c072f7f9c783b9a5c5d931aef
nam-ayeon <boalove284@naver.com>
2023-03-27 15:10:35 +0900

    파일업로드 상세 처리 1) 중복된 이름의 첨부파일 처리 2) 년/월/일 폴더 생성하기 3) 섬네일 이미지생성을 위한 라이브러리추가

--- BasicBoard/src/main/java/com/ayeon/goodWeb/UploadController.java
+++ BasicBoard/src/main/java/com/ayeon/goodWeb/UploadController.java
@@ -1,6 +1,9 @@
 package com.ayeon.goodWeb;
 
 import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.UUID;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.ctc.wstx.shaded.msv_core.grammar.xmlschema.Field;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -30,24 +34,45 @@ public class UploadController {
 
 		//업로드될폴더 
 		String uploadFolder = "/Users/nam-ayeon/Desktop/untitledfolder/temp";
+		
+		//makeFolder
+		File uploadPath = new File(uploadFolder, getFolder());
+		log.info(" -- upload Path: --  " + uploadPath);
+		if(uploadPath.exists() == false) {
+			
+			uploadPath.mkdirs();
+		}//make yyyy/MM/dd folder 
+		
+		
 
+		
 		for (MultipartFile multipartFile : uploadFile) {
 
-			log.info("..............................................");
+			/*log.info("..............................................");
 			log.info("UploadFileName: " + multipartFile.getOriginalFilename());
 			log.info("UploadFileSize: " + multipartFile.getSize());
+			*/
+			
 			//업로드될파일이름 
 			String uploadFileName = multipartFile.getOriginalFilename();
 			
 			uploadFileName = uploadFileName.substring(uploadFileName.lastIndexOf("\\") + 1);
 
 			
-			log.info("only file name : " + uploadFileName);
-			File saveFile = new File(uploadFolder,uploadFileName);
+//			log.info("only file name : " + uploadFileName);
+
+			UUID uuid = UUID.randomUUID();
+			
+			uploadFileName = uuid.toString() + "_" + uploadFileName;
+			
+			
+			File saveFile = new File(uploadPath,uploadFileName);
 
 			try {
 
 				multipartFile.transferTo(saveFile);
+				
+				
 			} catch (Exception e) {
 				// TODO: handle exception
 				log.error(e.getMessage());
@@ -55,5 +80,16 @@ public class UploadController {
 
 		}
 	}
+	
+	private String getFolder() {
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		Date date = new Date();
+		String str = sdf.format(date);
+		return str.replace("-", File.separator);
+	
+		}
+	
+	
 
 }
