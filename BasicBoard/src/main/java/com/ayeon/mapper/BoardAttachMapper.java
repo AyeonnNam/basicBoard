17b42a4e117721f5149cee7115d38c08873fe342
nam-ayeon <boalove284@naver.com>
2023-03-29 18:21:54 +0900

    매퍼 작성 (수정은 없음)

---
+++ BasicBoard/src/main/java/com/ayeon/mapper/BoardAttachMapper.java
@@ -0,0 +1,17 @@
+package com.ayeon.mapper;
+
+import java.util.List;
+
+import com.ayeon.domain.BoardAttachVO;
+
+public interface BoardAttachMapper {
+
+	public void insert(BoardAttachVO vo);
+	
+	public void delete(String uuid);
+	
+	public List<BoardAttachVO> findByBno(Long bno);
+	
+	
+		
+}
