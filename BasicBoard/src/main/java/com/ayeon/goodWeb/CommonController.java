83e89dc2b35077fb99368dc7e9d533dbeff0c025
nam-ayeon <boalove284@naver.com>
2023-04-18 01:01:28 +0900

    커스텀 로그인 페이지

--- BasicBoard/src/main/java/com/ayeon/goodWeb/CommonController.java
+++ BasicBoard/src/main/java/com/ayeon/goodWeb/CommonController.java
@@ -18,4 +18,21 @@ public class CommonController {
 		
 		
 	}
+	
+	@GetMapping("/customLogin")
+	public void loginInput(String error, String logout, Model model) {
+		
+		
+		log.info("error: " + error);
+		log.info("logout: " + logout);
+		
+		if(error != null) {
+			model.addAttribute("error","Login Error Check Your Account");
+			
+		}
+		if(logout != null) {
+			model.addAttribute("logout","Logout!!");
+			
+		}
+	}
 }
--- BasicBoard/src/main/java/com/ayeon/security/CustomAccessDeniedHandler.java
+++ BasicBoard/src/main/java/com/ayeon/security/CustomAccessDeniedHandler.java
@@ -1,27 +1,29 @@
 package com.ayeon.security;
 
-import java.nio.file.AccessDeniedException;
+import java.io.IOException;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.web.access.AccessDeniedHandler;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-public class CustomAccessDeniedHandler {
+public class CustomAccessDeniedHandler implements AccessDeniedHandler{@Override
+	public void handle(HttpServletRequest request, HttpServletResponse response,
+			org.springframework.security.access.AccessDeniedException accessDeniedException)
+			throws IOException, ServletException {
 	
 	
-	public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessException ) 
+			log.error("Access Denied Hanlder");
+			log.error("Redirect.........");
+			response.sendRedirect("/accessError");
 			
-			throws Exception {
-		
-		log.error("Accedd Denied Handler");
-		
-		log.error("Redirect....");
-				
-		response.sendRedirect("/accessError");
-				
 	}
+	
+	
 	
 	
 }
