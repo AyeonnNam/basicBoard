2e653982ccd8a64174e55407c70156b8d2dfde67
nam-ayeon <boalove284@naver.com>
2023-04-19 15:29:02 +0900

    tbl_member_auth insertTest

--- BasicBoard/src/test/java/com/ayeon/security/MemberTests.java
+++ BasicBoard/src/test/java/com/ayeon/security/MemberTests.java
@@ -28,7 +28,7 @@ public class MemberTests {
 	@Setter(onMethod_ = @Autowired)
 	private DataSource ds;
 	
-	@Test
+	//@Test
 	public void testInsertMember() {
 		
 		String sql = "insert into tbl_member(userid, userpw, username) values (?,?,?)";
@@ -72,4 +72,48 @@ public class MemberTests {
 		
 	}
 	
+	@Test
+	public void testInsertAuth() {
+		
+		String sql = "insert into tbl_member_auth(userid, auth) values (?,?)";
+		
+		for(int i=0; i<100; i++) {
+			
+			Connection conn = null;
+			PreparedStatement pstmt = null;
+			
+			try {
+				
+				conn = ds.getConnection();
+				pstmt = conn.prepareStatement(sql);
+				
+				if(i<80) {
+					
+					pstmt.setString(1, "user" + i);
+					pstmt.setString(2, "ROLE_USER");
+				}else if(i<90) {
+					pstmt.setString(1, "manager" + i);
+					pstmt.setString(2, "ROLE_MANAGER");
+					
+				}else {
+					
+					pstmt.setString(1, "admin" + i);
+					pstmt.setString(2, "ROLE_ADMIN");
+				}
+				pstmt.executeUpdate();
+				
+			}catch (Exception e) {
+			e.printStackTrace();
+			
+			}finally {
+				if(pstmt !=null) {try {pstmt.close();} catch (Exception e) {}}
+				if(conn != null) {try { conn.close();} catch (Exception e) {}}
+			}
+			
+		}//end for // 
+		
+		
+	}
+
+
 }
